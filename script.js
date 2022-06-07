d05e19798c8cbdceaeecddd6a876b9b3b3d9d417
Nicolas Vandenbruaene <nicolas.vandenbruaene@gmail.com>
2022-06-07 17:16:38 +0200

    JS Working, CSS on the way

---
+++ script.js
@@ -0,0 +1,15 @@
+const URL = "https://api.adviceslip.com/advice"
+
+// fonction asynchrone pour fetcher et parser les datas de l'api
+const loadData = async () => {
+    let res = await fetch(URL)
+    let data = await res.json()
+    document.getElementById("advice-id").innerHTML= `
+    <h3>advice # ${data.slip.id}</h3>
+    `
+    document.getElementById("advice-html").innerHTML = `
+        <p>${data.slip.advice}</p>
+    `
+}
+
+loadData()
