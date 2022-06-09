d030ed9a9a9e2dbf08ad3dad9859fa88c515ec05
Nicolas Vandenbruaene <nicolas.vandenbruaene@gmail.com>
2022-06-09 12:20:44 +0200

    Some changes to the CSS for better links in the footing + screenshots for github Readme

--- script.js
+++ script.js
@@ -2,14 +2,19 @@ const URL = "https://api.adviceslip.com/advice"
 
 // fonction asynchrone pour fetcher et parser les datas de l'api
 const loadData = async () => {
-    let res = await fetch(URL)
-    let data = await res.json()
-    document.getElementById("advice-id").innerHTML= `
-    <h3>ADVICE # ${data.slip.id}</h3>
-    `
-    document.getElementById("advice-html").innerHTML = `
-        <p>"${data.slip.advice}"</p>
-    `
+    try {
+        let res = await fetch(URL)
+        let data = await res.json()
+        document.getElementById("advice-id").innerHTML= `
+        <h3>ADVICE # ${data.slip.id}</h3>
+        `
+        document.getElementById("advice-html").innerHTML = `
+            <p>"${data.slip.advice}"</p>
+        `
+    }
+   catch {
+       console.error(err)
+   }
 }
 
 loadData()
